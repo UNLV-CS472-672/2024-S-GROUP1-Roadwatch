36801c0b6ff6b0941628817e120504747aa4e165
Aligary <49879221+Aligary@users.noreply.github.com>
2024-03-02 21:34:18 -0800

    Created InputField components (#13)
    * Created InputField components
    
    * implemented suggestions from PR
    
    * changed the expected value of PasswordFieldProp
    
    * implemented review suggestions

---
+++ src/components/PasswordField/PasswordField.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import styles from './PasswordField.module.scss';
+import hide from '../../assets/hidePasswordIcon.png';
+import show from '../../assets/showPasswordIcon.png'; 
+
+/**
+ * Parameters for the password field
+ */
+interface PasswordFieldProps {
+    /**
+     * String of the header for the textbox.
+     * @example
+     * header={"Enter Password"}
+     */
+    header: string;
+    /** Variable to set the input value of the password. */
+    setInputValue: (value: string) => void;
+}
+
+/***
+ * Creates a password field with a header
+ */
+
+const PasswordField: React.FC<PasswordFieldProps> = ({ header, setInputValue }: PasswordFieldProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div>
+            <div className={styles['PasswordField__header']}>{header}</div>
+            <div className={styles['PasswordField__container']}>
+                <input
+                    className={styles['PasswordField__textBox']}
+                    type={showPassword ? 'text' : 'password'}
+                    onChange={handleChange}
+                    placeholder={`Enter ${header}`}
+                />
+                <button 
+                    className={styles['PasswordField__showPasswordBtn']}
+                    onClick={togglePasswordVisibility}
+                >
+                    <img src={showPassword ? hide : show} height={"100%"} alt={showPassword ? "hide password" : "show password"}/>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default PasswordField;
