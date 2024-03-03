577e3675c156f0cf0d54130d6b7e1d245d42a9b6
Trevor <trevortippery221@gmail.com>
2024-03-02 18:26:57 -0800

    implemented suggestions from PR

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from 'react'
 import styles from './Loading.module.scss';
 import logo from '../../assets/Temp_Roadwatch_logo.svg';
-// import logo2 from '../../assets/Temp_Roadwatch_logo-2.svg'
 
 export function Loading(): JSX.Element {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 4000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
     <div className={styles['Loading__container']}>
-      {isLoading && (
-        <img src={logo} alt="Loading..." className={styles['Loading__logo']} />
-      )}
+      <img src={logo} alt="Loading..." className={styles['Loading__logo']} />
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default } from './Navbar/Navbar';
+export * from './Loading/Loading';
