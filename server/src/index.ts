111a55ce4d96a12affb58b8aa63ac286a17e87d8
Joshua <link7844@gmail.com>
2024-03-08 07:44:57 -0800

    add all user routes and eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from "cors";
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { connect } from './config/database';
-import userRoutes from './routes/User'
+import userRoutes from './routes/User';
 
 dotenv.config();
 const app = express();
@@ -11,11 +11,11 @@ const port = process.env.PORT || 3000;
 connect();
 
 // Middleware
-app.use(cors());  // cors is needed for security otherwise some things are just inaccessbile
+app.use(cors()); // cors is needed for security otherwise some things are just inaccessbile
 app.use(bodyParser.json()); // this is needed to parse body of requests
 
 // routes
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 
 // server start
 app.listen(port, () => {
