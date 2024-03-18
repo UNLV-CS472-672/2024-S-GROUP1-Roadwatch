6e19ce38e37ac89251dd159bbc6a668667d84dc3
Tim McAllister <tbmcallister2002@gmail.com>
2024-03-18 16:15:20 -0700

    created Location Page, added MUI and icons

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,70 @@
 import styles from './SignUp.module.scss';
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export default function SignUp(): JSX.Element {
-  return (
-    <div className={styles['SignUp']}>
-      <p>Sign Up Page</p>
+  return ( 
+    <div className={styles['SignUp']}> 
+      {/* create the header */}
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+        <Stack direction="row" spacing={1} alignItems={"center"} justifyContent={"space-evenly"}>
+          <IconButton aria-label='back'>
+            <ArrowBackIosIcon/>
+          </IconButton>
+          <Typography variant="h4" gutterBottom align="center" sx={{ flexGrow: 1}}>
+            Location
+          </Typography>
+        </Stack>
+      </Box>
+
+      {/* for mobile to desktop scaling */}
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      {/* create the input fields and the titles for the fields */}
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+        <Stack spacing={2}>
+            <Typography variant="body1" gutterBottom>
+                Address
+            </Typography>
+            <TextField id="outlined-search" label="Enter Address" type="search" />
+
+            <Typography variant="body1" gutterBottom>
+                City
+            </Typography>
+            <TextField id="outlined-search" label="Enter City" type="search" />
+
+            <Typography variant="body1" gutterBottom>
+                State
+            </Typography>
+            <TextField id="outlined-search" label="Enter State" type="search" />
+
+            <Typography variant="body1" gutterBottom>
+                Zip Code
+            </Typography>
+            <TextField id="outlined-search" label="Enter Zip" type="search" />
+
+            {/* Add a continue button 
+                color can be changed if need be, default is blue*/}
+            <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
+              <Button variant="contained" size="large" color="success">
+                Continue
+              </Button>
+            </Box>
+        </Stack>
+      </Box>
     </div>
-  );
+  ); 
 }
