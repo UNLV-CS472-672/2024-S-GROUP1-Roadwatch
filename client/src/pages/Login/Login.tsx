07251d070d9889c328b714de2bbd0ab6685cb710
dafneMena <dafneg2306@icloud.com>
2024-03-19 17:37:32 -0700

    Login Page created

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { useState } from 'react';
 import styles from './Login.module.scss';
@@ -5,17 +8,26 @@ import { Button } from '@mui/material';
 import { useLoginMutation } from '@/store';
 import { useNavigate } from 'react-router-dom';
 import { TextField, PasswordField } from '@/components';
+import logo from '../../assets/Updated_RoadWatch_Logo.svg'; 
 
 export default function Login(): JSX.Element {
+   // State management for user input and password
   const [userInput, setUserInput] = useState('');
   const [password, setPassword] = useState('');
+
+  // Mutation hook for login functionality
   const [login] = useLoginMutation();
+
+  // Navigation hook for redirecting users
   const navigate = useNavigate();
 
+  // Function to handle login process
   const handleLogin = async () => {
     try {
+      // Attempt login
       const res = await login({ userInput, password });
 
+      // If response contains data, extract user ID and navigate to home page
       // this is very annoying type guarding
       if ('data' in res) {
         const { user_id } = res.data;
@@ -24,16 +36,34 @@ export default function Login(): JSX.Element {
         }
       }
     } catch (err) {
+
+      // Log any errors to the console
       console.log(err);
     }
   };
 
+  // Render login form
   return (
-    <div className={styles['Login']}>
-      <p>Login Page</p>
-      <TextField header="Username or Email" setInputValue={setUserInput} />
-      <PasswordField header="Password" setInputValue={setPassword} />
-      <Button onClick={handleLogin}>Login</Button>
+    <div><p>Login Page</p>
+      <div className={styles['Login__LoginContainer']}>
+        <div className={styles['Login__whiteContainer']}>
+          <div className={styles['Login__logo']}> <img src = {logo} alt = {"Roadwatch Logo"}/></div>
+          <TextField header="Username or Email" setInputValue={setUserInput} />
+          <PasswordField header="Password" setInputValue={setPassword} />
+          <div className={styles['Login__textCentered']}>
+            <Button onClick={handleLogin}>Login</Button>
+            <br /><br />
+            <b>Forgot Password</b>
+            <br /><br />
+            <div className={styles['Login__registerHereContainer']}>
+                <p> {"Don't have an account?"}
+                {/* Once 'register page' is completed update to <a> element to create a hyperlink.
+                    ex. <a href="/register" className={styles.Login__registerHereBtn}>Register here</a> */}
+                <span className={styles['Login__registerHereBtn']}> Register here</span></p> 
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
