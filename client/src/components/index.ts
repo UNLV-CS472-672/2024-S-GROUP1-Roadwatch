cb78f6926717cc9d3c106bf0b68ebd2b17f09b43
Trevor Tippery <69101935+trevortippery@users.noreply.github.com>
2024-03-20 16:26:36 -0700

    ResetPassword component (#39)
    * ResetPassword component added
    
    * Fixed index.ts file

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as PasswordField } from './PasswordField/PasswordField';
 export { default as TextField } from './TextField/TextField';
 export { default as Loading } from './Loading/Loading';
 export { default as CustomButton } from './CustomButton/CustomButton';
+export { default as ResetPassword } from './ResetPassword/ResetPassword';
