a505b84d7815a353e72067dff6e577fd5f1eb685
Timmy McAllister <123442275+tbmcallister@users.noreply.github.com>
2024-03-22 13:45:29 -0700

    LocationNotification Component Front End (#45)
    * implemented LocationNotification Component
    
    * comp appears at the front of the screen(positive Z)
    
    * suggestions per PR

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,4 +4,5 @@ export { default as TextField } from './TextField/TextField';
 export { default as Loading } from './Loading/Loading';
 export { default as CustomButton } from './CustomButton/CustomButton';
 export { default as ResetPassword } from './ResetPassword/ResetPassword';
+export { default as LocationNotification } from './LocationNotification/LocationNotification'
 export { default as ResetPasswordVerify } from './ResetPasswordVerify/ResetPasswordVerify';
