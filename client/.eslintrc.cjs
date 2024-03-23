b60926d2cab144c9e1107b0069205664dd6f0ccb
Tim McAllister <tbmcallister2002@gmail.com>
2024-03-23 12:07:42 -0700

    split up linting to client/server

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
-  env: { browser: true, es2021: true },
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
