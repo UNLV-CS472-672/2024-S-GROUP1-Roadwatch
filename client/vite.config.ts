99d973e6890a97a70e9302964f49f688a771b67d
Jordan King <kingj32@unlv.nevada.edu>
2024-03-25 14:03:18 -0700

    Configured the Web App to be PWA Installable (#60)
    * Added base icon images of multiple sizes.
    
    * Added files autogenerated by `vite-plugin-pwa`.
    
    * Added `vite-plugin-pwa`.
    
    * Added some style checks to be ignored by stylelint.
    
    * Added Apple touch icons and favicons.
    
    * Configured `index.html` with necessary metadata to make the web app PWA installable.
    
    * Added `vite-plugin-pwa` to `vite.config.ts` and configured the `manifest` field to make our web app PWA installable.
    
    * Configured `robots.txt` with sufficient data to allow our web app to be crawled and indexed by Google.
    
    * Removed unnecessary SVG created from the original Vite setup.
    
    * Added missing brace that was causing the app to crash.
    
    * Prevented the Home screen from scrolling along the x-axis; allows our app to be PWA installable.
    
    * Adjusted SVG parameters to increase size and remove extraneous CSS.
    
    * Removed `dev-dist` directory and added it to `.gitignore`.
    
    * Removed `serve` command from `package.json` since it was used for internal testing only.
    
    ---------
    
    Co-authored-by: Jordan King <jordan.king@creditone.com>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,54 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'favicon.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Roadwatch',
+        short_name: 'Roadwatch',
+        description:
+          'Roadwatch revolutionizes how communities interact with and navigate their local road systems by providing a combined platform for reporting road conditions, alerting users to hazards, allowing community input, and influencing road infrastructure improvements.',
+        theme_color: '#FB7110',
+        icons: [
+          {
+            src: '/icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icons/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     // Ensures a CSS file is emitted during `build` step.
     cssCodeSplit: false, // TODO: Reassess if necessary towards end of project.
