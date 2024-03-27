1b87549ceaafadaf57e496f88e03984d1ca05198
Arthur Valdez <135791087+sooble13@users.noreply.github.com>
2024-03-26 19:05:31 -0700

    Instructions page (#65) (#79)
    * Instructions Page Updates
    
    Added two new files:
    - Instructions.tsx
    - Instructions.module.scss
    
    Also added the route to Instructions in main.tsx.
    
    * Updated Instructions Page
    
    Started the basic outline of the Instructions Page.
    
    * Updated instructionsPage
    
    There is now a fully functioning instructions page.
    
    * Update package.json
    
    Added the mui carousel package
    
    * Update Instructions.tsx
    
    Removed unused image imports.
    
    ---------
    
    Co-authored-by: valdea13 <135791087+valdea13@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Chat, Community, Home, Login, Profile, SignUp, GeneralInfo, CreateAccount } from '@/pages';
+import {
+  Chat,
+  Community,
+  Home,
+  Login,
+  Profile,
+  SignUp,
+  GeneralInfo,
+  CreateAccount,
+  Instructions,
+} from '@/pages';
 import { PrivateRoute } from '@/utils';
 
 // Collection of paths for the site.
@@ -44,6 +54,10 @@ const router = createBrowserRouter([
     path: '/general-info',
     element: <GeneralInfo />,
   },
+  {
+    path: '/instructions',
+    element: <Instructions />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -53,4 +67,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Provider>
   </React.StrictMode>
 );
-
