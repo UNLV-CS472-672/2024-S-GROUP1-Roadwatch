2a5e7f6b1a7e622af15166483c14ec9e00fc8d86
Arthur Valdez <135791087+sooble13@users.noreply.github.com>
2024-03-26 21:42:44 -0700

    Merging main into issue #19 branch. (#82)
    * Instructions page (#65)
    
    * Instructions Page Updates
    
    Added two new files:
    - Instructions.tsx
    - Instructions.module.scss
    
    Also added the route to Instructions in main.tsx.
    
    * Updated Instructions Page
    
    Started the basic outline of the Instructions Page.
    
    * Updated instructionsPage
    
    There is now a fully functioning instructions page.
    
    * Update package.json
    
    Added the mui carousel package
    
    * Update Instructions.tsx
    
    Removed unused image imports.
    
    ---------
    
    Co-authored-by: valdea13 <135791087+valdea13@users.noreply.github.com>
    
    * Home Page and Components Styling Overhaul (#64)
    
    * Improved style of the home page.
    Reworked Navbar component.
    Added Navbar component to pages.
    Improved styling of header component.
    
    * added navigation bar to generalInfo page.
    
    * Added ESLint recommendations.
    
    * Additional fixes to ESLint
    
    * Additional fixes to ESLint
    
    ---------
    
    Co-authored-by: valdea13 <135791087+valdea13@users.noreply.github.com>
    Co-authored-by: Juan Moreno <56707302+morenj@users.noreply.github.com>

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
 import styles from './Header.module.scss';
+import { CustomButton } from '@/components';
+import notification_icon from 'src/assets/icons/notification-icon.svg';
+import help_icon from 'src/assets/icons/help-icon.svg';
 
 interface HeaderProps {
   userName?: string; // Optional prop
 }
 
 export default function Header(props: HeaderProps): JSX.Element {
-  
   // Destructure the userName prop with a default value
-  const { userName = "Jane Doe" } = props; 
+  const { userName = 'Jane Doe' } = props;
+
+  // void function to throw an error
+  function throwError(): void {
+    // alert the user that the function is not implemented
+    alert('Function not implemented.');
+    throw new Error('Function not implemented.');
+  }
 
   return (
     <div className={styles['Header__wrapper']}>
@@ -22,18 +31,46 @@ export default function Header(props: HeaderProps): JSX.Element {
         {/* Welcome message and logout button */}
         <div className={styles['Header__vertical_container']}>
           <p className={styles['Header__heading_text']}>Welcome, {userName}</p>
-          <button className={styles['Header__logout_button']}>Logout</button>
+          <CustomButton onClick={throwError}>Logout</CustomButton>
         </div>
       </div>
 
       {/* Quick action buttons */}
       <div className={styles['Header__horizontal_container']}>
         <div className={styles['Header__quick_action_wrapper']}>
-          <div className={styles['Header__block']}>
-            <button className={styles['Header__quick_action_button']}>Help</button>
+          <div className={styles['Header__block']}
+            role="button"
+            onClick={throwError}
+            tabIndex={0}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                throwError();
+              }
+            }} // Adds keyboard interaction
+          >
+            {/* <CustomButton onClick={throwError} >Help</CustomButton> */}
+            <img
+              src={notification_icon}
+              alt="Notification Icon"
+              className={styles['Header__quick_action_button']}
+            />
           </div>
-          <div className={styles['Header__block']}>
-            <button className={styles['Header__quick_action_button']}>Notifications</button>
+          <div className={styles['Header__block']}
+            role="button"
+            onClick={throwError}
+            tabIndex={0}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                throwError();
+              }
+            }} // Adds keyboard interaction
+          >
+            {/* <CustomButton onClick={throwError}>Notifications</CustomButton> */}
+            <img
+              src={help_icon}
+              alt="Help Icon"
+              className={styles['Header__quick_action_button']}
+            />
           </div>
         </div>
       </div>
