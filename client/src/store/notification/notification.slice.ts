17a03c4221f9f445a556c19989e407a1da4da8d1
Jordan King <kingjordan2718@gmail.com>
2024-04-04 00:09:47 -0700

    Added JSDocs to the `useNotificationSubscriptionMutation` hook.

--- client/src/store/notification/notification.slice.ts
+++ client/src/store/notification/notification.slice.ts
@@ -15,7 +15,7 @@ interface ISubscription {
   subscription: PushSubscription;
 }
 
-export const notification = apiSlice.injectEndpoints({
+const notification = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     sendNotification: builder.query<INotficationResponse, INotification>({
       query: ({ id, message }) =>
@@ -37,32 +37,44 @@ export const notification = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useLazySendNotificationQuery, useSaveSubscriptionMutation } = notification;
+export const { useLazySendNotificationQuery } = notification;
 
-export const useNotificationSubscriptionMutation = async ({ id }: Pick<ISubscription, 'id'>) => {
-  const [subscribe] = useSaveSubscriptionMutation();
+/**
+ * Provides a method for subscribing the user to receive push notifications.
+ * @returns A tuple containing the trigger for initiating the subscription mutation and an object containing response information.
+ *
+ * @see {@link https://redux-toolkit.js.org/rtk-query/usage/mutations}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/PushManager/subscribe}
+ */
+export const useNotificationSubscriptionMutation = () => {
+  const [subscribe, mutationData] = notification.useSaveSubscriptionMutation();
 
-  // Check notification permissions before proceeding.
-  if (Notification.permission === 'denied') return;
-  if (Notification.permission === 'default') {
-    await Notification.requestPermission().catch((e) => console.error(e));
-  }
+  const notificationSubscriptionMutation = async ({ id }: Pick<ISubscription, 'id'>) => {
+    // Check notification permissions before proceeding.
+    if (Notification.permission === 'denied') return;
+    if (Notification.permission === 'default') {
+      await Notification.requestPermission().catch((e) => console.error(e));
+    }
 
-  // If the service worker hasn't been registered yet, don't attempt subscription.
-  const registration = await navigator.serviceWorker.getRegistration();
-  if (!registration) return;
+    // If the service worker hasn't been registered yet, don't attempt subscription.
+    const registration = await navigator.serviceWorker.getRegistration();
+    if (!registration) return;
 
-  try {
-    const applicationServerKey = Buffer.from(
-      'BOF8HhjS9CbQ4Qf7SvJ7wehXHUveQ1iNBSkZSYifNIWVVmgadYmye5vy7wmAkFYVpIHnTXhyc5N6myssnwRcono',
-      'base64'
-    );
-    const subscription: PushSubscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true, // Makes user see every notification sent.
-      applicationServerKey,
-    });
-    await subscribe({ id, subscription });
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+      // MUST encode VAPID public key into Base64.
+      const applicationServerKey = Buffer.from(
+        import.meta.env.VITE_NOTIFICATION_PUBLIC_KEY as string,
+        'base64'
+      );
+      const subscription: PushSubscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true, // Makes user see every notification sent.
+        applicationServerKey, // MUST be applied for all Chromium browsers.
+      });
+      await subscribe({ id, subscription });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [notificationSubscriptionMutation, mutationData] as const;
 };
