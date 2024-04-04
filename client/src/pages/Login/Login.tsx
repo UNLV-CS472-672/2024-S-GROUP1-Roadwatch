d5f5256d6f4aec85f5c61a872e305342bf8e2350
Jordan King <kingjordan2718@gmail.com>
2024-04-04 00:27:19 -0700

    Removed all test notification buttons from the Login page.

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -4,11 +4,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { useState } from 'react';
 import styles from './Login.module.scss';
-import { useCreateUserMutation, useLoginMutation, useGetUserQuery } from '@/store';
+import { useLoginMutation } from '@/store';
 import { useNavigate } from 'react-router-dom';
 import { TextField, PasswordField, CustomButton } from '@/components';
 import logo from '../../assets/Updated_RoadWatch_Logo.svg';
-import { useLazySendNotificationQuery, useSaveSubscriptionMutation } from '@/store';
 
 export default function Login(): JSX.Element {
   // State management for user input and password
@@ -42,45 +41,6 @@ export default function Login(): JSX.Element {
     }
   };
 
-  const [sendNotification] = useLazySendNotificationQuery();
-  const [subscribe] = useSaveSubscriptionMutation();
-
-  const handleNotificationRequest = async () => {
-    await Notification.requestPermission().catch((e) => console.error(e));
-  };
-
-  const handleNotificationSend = async () => {
-    console.log(data?.id);
-    await sendNotification({ id: data?.id as string, message: 'Hello Jordan!' });
-  };
-
-  const handleNotificationSubscription = async () => {
-    if (Notification.permission !== 'granted') return;
-
-    const registration = await navigator.serviceWorker.getRegistration();
-
-    if (!registration) return;
-
-    try {
-      const applicationServerKey = Buffer.from(
-        'BOF8HhjS9CbQ4Qf7SvJ7wehXHUveQ1iNBSkZSYifNIWVVmgadYmye5vy7wmAkFYVpIHnTXhyc5N6myssnwRcono',
-        'base64'
-      );
-      const subscription: PushSubscription = await registration.pushManager.subscribe({
-        userVisibleOnly: true, // Makes user see every notification sent.
-        applicationServerKey,
-      });
-      await subscribe(subscription);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleUnregisterServiceWorker = async () => {
-    const registration = await navigator.serviceWorker.getRegistration();
-    await registration?.unregister();
-  };
-
   // Render login form
   return (
     <div className={styles['Login__LoginContainer']}>
@@ -108,10 +68,6 @@ export default function Login(): JSX.Element {
           </div>
         </div>
       </div>
-      <button onClick={handleUnregisterServiceWorker}>Unregister SW</button>
-      <button onClick={handleNotificationRequest}>Request Notification Permission</button>
-      <button onClick={handleNotificationSubscription}>Subscribe to Notifications</button>
-      <button onClick={handleNotificationSend}>Send Notification</button>
     </div>
   );
 }
