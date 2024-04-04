1818d1dafc26422cf485495f42cf007d9690d702
Timmy McAllister <tbmcallister2002@gmail.com>
2024-04-04 13:42:38 -0700

    re-adding validateToken

--- server/src/routes/Marker.ts
+++ server/src/routes/Marker.ts
@@ -5,10 +5,10 @@ import { validateToken } from '../middlewares';
 const router = express.Router();
 
 // define routes
-router.get('/', markerController.getMarkers);
+router.get('/', validateToken, markerController.getMarkers);
 
-router.post('/', markerController.saveMarker);
+router.post('/', validateToken, markerController.saveMarker);
 
-router.delete('/', markerController.deleteMarker);
+router.delete('/', validateToken, markerController.deleteMarker);
 
 export default router; 
