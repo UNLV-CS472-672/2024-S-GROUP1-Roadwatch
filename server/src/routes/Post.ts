56902a461261fb50e8d96c7e3e81287efedf92f9
Juan Moreno <56707302+morenj@users.noreply.github.com>
2024-04-22 20:06:35 -0700

    Post server endpoints (#132)
    * Progress on adding endpoints for posts.
    
    * Added savePost
    
    * Finished implementing endpoints for post
    
    * Cleaned up code and added AI documentation

---
+++ server/src/routes/Post.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import * as postController from '../controllers/PostController';
+import { validateToken } from '../middlewares';
+
+const router = express.Router();
+
+router.get(
+  '/all-posts',
+  validateToken,
+  postController.getAllPosts
+);
+
+router.post(
+  '/save-post',
+  validateToken,
+  postController.savePost
+);
+
+router.delete(
+  '/delete-post/:id',
+  validateToken,
+  postController.deletePost
+);
+
+export default router;
