454c8d3a52aa287748061d700356372a2c7a79b6
Aligary <aligaryjr@gmail.com>
2024-04-24 14:49:36 -0700

    Unit test for Forgot page

--- client/src/pages/Forgot/Forgot.tsx
+++ client/src/pages/Forgot/Forgot.tsx
@@ -3,8 +3,8 @@ import { ForgotPassword } from '@/components';
 
 export default function Reset(): JSX.Element {
     return (
-        <div className={styles['Forgot']}>
-            <ForgotPassword />
+        <div className={styles['Forgot']} data-testid={'Forgot-container'}>
+            <ForgotPassword data-testid={'ForgotPassword-component'}/>
         </div>
     );
 }
---
+++ client/tests/pages/Forgot/Forgot.test.tsx
@@ -0,0 +1,30 @@
+import { Forgot } from "@/pages"
+import { describe, test, assert, afterEach} from "vitest";
+import { render, screen, cleanup } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { BrowserRouter } from 'react-router-dom';
+
+describe("Forgot.tsx", () => {
+    const initialState = { output: 10 };
+    const mockStore = configureStore();
+    let store;
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    test("ForgotPassword component renders", () => {
+        store = mockStore(initialState);
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Forgot />
+                </BrowserRouter>
+            </Provider>
+        );
+        const element = screen.getByTestId('Forgot-container');
+        assert(element);
+    });
+
+});
